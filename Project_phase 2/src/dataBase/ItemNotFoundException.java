b02df773d3e50c196a254a0372395e2b792310f0
mahmoud <mahmoodkhatib@hotmail.com>
2024-04-09 17:46:21 +0300

    Seconds Commit

--- Project_phase 2/src/dataBase/ItemNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package dataBase;
-
-public class ItemNotFoundException extends Exception {
-	//this exception is for the items that are not found in an array or array list
-	public ItemNotFoundException(String title) {
-		super(title + " Does Not Exist");
-	}
-}
